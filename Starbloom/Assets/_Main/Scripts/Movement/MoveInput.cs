e99d0bc112847b090483283bfe7b3427a39d3825
Tom <contact.tbrooks@gmail.com>
2018-05-14 00:01:35 -0400

    Doin' some good ol' locowhatsit work

---
+++ Starbloom/Assets/_Main/Scripts/Movement/MoveInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Locomotion))]
+public class MoveInput : MonoBehaviour
+{
+	public bool InputDetected = false;
+	protected Locomotion MoveController;
+
+	void Update()
+	{
+		if (null == MoveController)
+			MoveController = GetComponent<Locomotion>();
+
+		DG_PlayerInput.Player MP = QuickFind.InputController.MainPlayer;
+		if (MP.VerticalAxis != 0 || MP.HorizontalAxis != 0)
+			InputDetected = true;
+
+		Vector3 inputVec = new Vector3(MP.HorizontalAxis, 0f, MP.VerticalAxis);
+		inputVec = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f) * inputVec; 
+
+		MoveController.SetInput( inputVec );
+	}
+}
