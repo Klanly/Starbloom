f5697c593ad71ec5aa44a3921e1726d0e8c6a0a9
Tom <contact.tbrooks@gmail.com>
2018-06-04 19:33:08 -0400

    Garbage people voodoo garbage

---
+++ Starbloom/Assets/_Main/Scripts/NPC/NPCMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Locomotion))]
+public class NPCMovement : MonoBehaviour
+{
+	public void Update()
+	{
+
+	}
+}
